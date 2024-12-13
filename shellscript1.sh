a6f6494d36aa8ee064edcb105a78d027d2133086
Akshitha Rani Chettam <157853913+Akshitha0129@users.noreply.github.com>
2024-12-13 13:21:21 -0600

    Add files via upload

---
+++ shellscript1.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+# This is a comment!
+echo "Hello      World"       # This is a comment, too!
+echo "Hello World"
+echo "Hello * World"
+echo Hello * World
+echo Hello      World
+echo "Hello" World
+echo Hello "     " World
+echo "Hello "*" World"
+echo `hello` world
+echo 'hello' world
+
+# second question
+
+#!/bin/sh
+MY_MESSAGE="Hello World"
+echo $MY_MESSAGE
+
+echo Hello       World
+
+echo *
+case.shtml escape.shtml first.shtml 
+functions.shtml hints.shtml index.shtml 
+ip-primer.txt raid1+0.txt
+echo *txt
+ip-primer.txt raid1+0.txt
+echo "*"
+*
+echo "*txt"
+*txt
+
+#example 3
+
+#!/bin/sh
+for i in 1 2 3 4 5
+do
+  echo "Looping ... number $i"
+done
+
+# examplee 4
+
+#!/bin/sh
+for i in hello 1 \* 2 goodbye 
+do
+  echo "Looping ... i is set to $i"
+done
+
+# example 5
+
+echo "Today is ${1:-a great day!}"
+
